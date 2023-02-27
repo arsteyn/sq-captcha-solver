51d4c6590a05a42577652f6ef11b826e7508b65b
Anatoly Rubinshteyn <a.l.rubinshteyn@gmail.com>
2023-02-27 21:10:14 +0200

    fix error when length != 4

--- app/main.py
+++ app/main.py
@@ -102,11 +102,7 @@ async def create_upload_file(file: UploadFile = File()):
             letter_image = result_image[y - 2:y + h + 2, x - 2:x + w + 2]
 
             # Re-size the letter image to 20x20 pixels to match training data
-            try:
-                letter_image = resize_to_fit(letter_image, 20, 20)
-            except:
-                print("!ssize.empty() in function 'resize'")
-                continue
+            letter_image = resize_to_fit(letter_image, 20, 20)
 
             # Turn the single image into a 4d list of images to make Keras happy
             letter_image = np.expand_dims(letter_image, axis=2)
